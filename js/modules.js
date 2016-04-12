fab4081fe74bf5b0b37c39a49894cb1db3863b2d
Andreas Argelius <andreas@argeli.us>
2016-04-12 18:48:24 +0900

    Some tutorials.

--- js/modules.js
+++ js/modules.js
@@ -2,7 +2,8 @@ app.modules = {
   'Getting started': [
     'Creating a page',
     'Stack navigation',
-    'Using tabs'
+    'Using tabs',
+    'Side menu'
   ],
   'React': [
     'Using the components'
