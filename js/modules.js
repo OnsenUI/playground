329943d85e3e36d13dcd180bea1608070f6c8e0c
Andreas Argelius <andreas@argeli.us>
2016-04-15 12:29:12 +0900

    Added more tutorials.

--- js/modules.js
+++ js/modules.js
@@ -5,8 +5,18 @@ app.modules = {
     'Using tabs',
     'Side menu',
     'Lists',
+    'Infinite lists',
+    'Pull to refresh',
     'Dialogs',
-    'Popover'
+    'Popover',
+    'Text input',
+    'Checkboxes',
+    'Radio buttons',
+    'Switches',
+    'Range sliders',
+    'Icons',
+    'Floating action button',
+    'Speed dial'
   ],
   'React': [
     'Using the components'
