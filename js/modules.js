cac628cf8d259cdacb3e105038efdba1127d1589
Fran Dios <frankdiox@gmail.com>
2016-05-19 19:29:50 +0900

    Added Reference tuts for Angular1.

--- js/modules.js
+++ js/modules.js
@@ -28,6 +28,28 @@ app.modules = {
       'Introduction': 'Introduction',
       'Data binding': 'Data binding',
       'Event handling': 'Event handling'
+    },
+    'Reference': {
+      'page': 'Creating a page',
+      'navigator': 'Stack navigation',
+      'tabbar': 'Using tabs',
+      'splitter': 'Side menu',
+      'list': 'Lists',
+      'lazy-repeat': 'Infinite lists',
+      'pull-hook': 'Pull to refresh',
+      'carousel': 'Carousel',
+      'dialog': 'Dialogs',
+      'popover': 'Popover',
+      'input': 'Form input',
+      'switch': 'Switches',
+      'button': 'Buttons',
+      'range': 'Range sliders',
+      'icon': 'Icons',
+      'if': 'Conditional element',
+      'progress': 'Progress',
+      'fab': 'Floating action button',
+      'speed-dial': 'Speed dial',
+      'ripple': 'Ripple effect'
     }
   },
   'angular2': {
