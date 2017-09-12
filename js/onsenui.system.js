76635521166df5191f330ed56a4e7e13a3d26c2a
Naoki Matagawa <naoki.m@asial.co.jp>
2017-09-12 20:18:35 +0900

    Support `ngx-onsenui` and update dependencies.

--- js/onsenui.system.js
+++ js/onsenui.system.js
@@ -1,38 +1,40 @@
 // Onsen UI SystemJS config
 
-var angular2onsenui = '';
+var ngxonsenui = '';
 
 if (window._isLocalDev) {
-  angular2onsenui = '../../OnsenUI/bindings/angular2';
+  ngxonsenui = '../../OnsenUI/bindings/angular2';
 } else if (window._onsNightlyBuild) {
-  angular2onsenui = 'https://crossorigin.me/https://circleci.com/api/v1/project/OnsenUI/OnsenUI/latest/artifacts/0/$CIRCLE_ARTIFACTS/angular2-onsenui';
+  ngxonsenui = 'https://crossorigin.me/https://circleci.com/api/v1/project/OnsenUI/OnsenUI/latest/artifacts/0/$CIRCLE_ARTIFACTS/angular2-onsenui';
 } else {
-  angular2onsenui = 'https://unpkg.com/angular2-onsenui' + (window._onsAngular2LibVersion ? ('@' + window._onsAngular2LibVersion) : '');
+  ngxonsenui = 'https://unpkg.com/ngx-onsenui' + (window._ngxOnsLibVersion ? ('@' + window._ngxOnsLibVersion) : '');
 }
 
-var libVersion = window._angular2LibVersion;
+var libVersion = window._ngxLibVersion;
+
+// Adapter modules for `ons` object
+System.registerDynamic('onsenui', [], false, function(require, exports, module) {
+  module.exports = window.ons;
+});
 
 System.config({
   map: {
-    'angular2-onsenui': angular2onsenui + '/dist/bundles/angular2-onsenui.umd.js' + (window._onsNightlyBuild ? '?branch=master&filter=successful' : ''),
+    'ngx-onsenui': ngxonsenui + '/dist/bundles/ngx-onsenui.umd.js' + (window._onsNightlyBuild ? '?branch=master&filter=successful' : ''),
     '@angular/core': `https://unpkg.com/@angular/core@${libVersion}/bundles/core.umd.min.js`,
     '@angular/compiler': `https://unpkg.com/@angular/compiler@${libVersion}/bundles/compiler.umd.min.js`,
     '@angular/common': `https://unpkg.com/@angular/common@${libVersion}/bundles/common.umd.min.js`,
     '@angular/forms': `https://unpkg.com/@angular/forms@${libVersion}/bundles/forms.umd.min.js`,
     '@angular/platform-browser': `https://unpkg.com/@angular/platform-browser@${libVersion}/bundles/platform-browser.umd.min.js`,
     '@angular/platform-browser-dynamic': `https://unpkg.com/@angular/platform-browser-dynamic@${libVersion}/bundles/platform-browser-dynamic.umd.min.js`,
-    'rxjs': 'https://unpkg.com/rxjs@5.0.0-beta.11',
+    'rxjs': 'https://unpkg.com/rxjs@5.4.3',
     'process': 'https://unpkg.com/process@0.11.9'
   },
   packages: {
-    'angular2-onsenui': {
+    'ngx-onsenui': {
       format: 'cjs',
     },
     'core-js': {
       main: 'index.js',
-      format: 'cjs'
-    },
-    'typescript': {
       format: 'cjs'
     },
     'app': {
